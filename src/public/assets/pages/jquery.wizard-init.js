d722090f5a0c13f92e2366e092b274d457d2b548
Your Name <phongit1995@gmail.com>
2020-05-29 07:03:30 +0000

    'phong'

--- src/public/assets/pages/jquery.wizard-init.js
+++ src/public/assets/pages/jquery.wizard-init.js
@@ -46,3 +46,94 @@ $(function() {
     $('.stepy-navigator').find('.button-next').addClass('btn btn-primary waves-effect waves-light');
     $('.stepy-step').find('.button-back').addClass('btn btn-default waves-effect pull-left');
 });
+
+//Edit form
+
+$(function() {
+    // Override defaults
+    $.fn.stepy.defaults.legend = false;
+    $.fn.stepy.defaults.transition = 'fade';
+    $.fn.stepy.defaults.duration = 200;
+    $.fn.stepy.defaults.backLabel = '<i class="mdi mdi-arrow-left-bold"></i> Back';
+    $.fn.stepy.defaults.nextLabel = 'Next <i class="mdi mdi-arrow-right-bold"></i>';
+
+
+    $('#default-wizard').stepy();
+
+    // Clickable titles
+    $("#wizard-clickable").stepy({
+        titleClick: true
+    });
+
+    // Stepy callbacks
+    $("#wizard-callbacks-edit").stepy({
+        next: function(index) {
+            // alert('Going to step: ' + index);
+            $("#addressBill").text($('#AddVn').val());
+            $("#phoneBill").text($('#PhoneVn').val())
+            $("#addressShip").text($('#AddJp').val());
+            $("#phoneShip").text($('#PhoneJp').val())
+            $("#ProductName").text($("#NameProduct").val());
+            $("#ProductNumber").text($("#QtyPro").val());
+            $("#ProductPrice").text($("#RatePro").val());
+            $("#ProductToTal").text($("#RatePro").val()*$("#QtyPro").val());
+        },
+        back: function(index) {
+            // alert('Returning to step: ' + index);
+        },
+        finish: function() {
+            // alert('Submit canceled.');
+            return false;
+        }
+    });
+
+    // Apply "Back" and "Next" button styling
+    $('.stepy-navigator').find('.button-next').addClass('btn btn-primary waves-effect waves-light');
+    $('.stepy-step').find('.button-back').addClass('btn btn-default waves-effect pull-left');
+});
+
+
+//Edit form nhan vien
+
+$(function() {
+    // Override defaults
+    $.fn.stepy.defaults.legend = false;
+    $.fn.stepy.defaults.transition = 'fade';
+    $.fn.stepy.defaults.duration = 200;
+    $.fn.stepy.defaults.backLabel = '<i class="mdi mdi-arrow-left-bold"></i> Back';
+    $.fn.stepy.defaults.nextLabel = 'Next <i class="mdi mdi-arrow-right-bold"></i>';
+
+
+    $('#default-wizard').stepy();
+
+    // Clickable titles
+    $("#wizard-clickable").stepy({
+        titleClick: true
+    });
+
+    // Stepy callbacks
+    $("#wizard-callbacks-edit-nhanvien").stepy({
+        next: function(index) {
+            // alert('Going to step: ' + index);
+            $("#addressBill").text($('#AddVn').val());
+            $("#phoneBill").text($('#PhoneVn').val())
+            $("#addressShip").text($('#AddJp').val());
+            $("#phoneShip").text($('#PhoneJp').val())
+            $("#ProductName").text($("#NameProduct").val());
+            $("#ProductNumber").text($("#QtyPro").val());
+            $("#ProductPrice").text($("#RatePro").val());
+            $("#ProductToTal").text($("#RatePro").val()*$("#QtyPro").val());
+        },
+        back: function(index) {
+            // alert('Returning to step: ' + index);
+        },
+        finish: function() {
+            // alert('Submit canceled.');
+            return false;
+        }
+    });
+
+    // Apply "Back" and "Next" button styling
+    $('.stepy-navigator').find('.button-next').addClass('btn btn-primary waves-effect waves-light');
+    $('.stepy-step').find('.button-back').addClass('btn btn-default waves-effect pull-left');
+});
